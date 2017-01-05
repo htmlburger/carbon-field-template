1b138fbcb881296310aef50f78a453245c9eda9b
Miroslav Mitev <miroslavmitev@gmail.com>
2017-01-05 11:45:12 +0200

    Fix the admin_enqueue_scripts method definition

--- FIELD_NAME_Field.php
+++ FIELD_NAME_Field.php
@@ -10,14 +10,14 @@ class FIELD_NAME_Field extends Field {
 
 	/**
 	 * to_json()
-	 * 
+	 *
 	 * You can use this method to modify the field properties that are added to the JSON object.
 	 * The JSON object is used by the Backbone Model and the Underscore template.
-	 * 
+	 *
 	 * @param bool $load  Should the value be loaded from the database or use the value from the current instance.
 	 * @return array
 	 */
-	function to_json( $load ) {
+	public function to_json( $load ) {
 		$field_data = parent::to_json( $load ); // do not delete
 
 		$field_data = array_merge( $field_data, array(
@@ -32,7 +32,7 @@ class FIELD_NAME_Field extends Field {
 	 *
 	 * Prints the main Underscore template
 	 **/
-	function template() {
+	public function template() {
 		?>
 		<input id="{{{ id }}}" type="text" name="{{{ name }}}" value="{{ value }}" class="regular-text" />
 
@@ -44,12 +44,12 @@ class FIELD_NAME_Field extends Field {
 
 	/**
 	 * admin_enqueue_scripts()
-	 * 
+	 *
 	 * This method is called in the admin_enqueue_scripts action. It is called once per field type.
 	 * Use this method to enqueue CSS + JavaScript files.
-	 * 
+	 *
 	 */
-	function admin_enqueue_scripts() {
+	public static function admin_enqueue_scripts() {
 		$dir = plugin_dir_url( __FILE__ );
 
 		# Enqueue JS
@@ -61,12 +61,12 @@ class FIELD_NAME_Field extends Field {
 
 	/**
 	 * admin_init()
-	 * 
+	 *
 	 * Called when the field is initialized. (back-end)
-	 * 
+	 *
 	 */
 	/*
-	function admin_init() {
+	public function admin_init() {
 		// Add another Underscore template
 		// $this->add_template( $this->get_type() . '-Description', array( $this, 'template_description' ) );
 
@@ -76,13 +76,13 @@ class FIELD_NAME_Field extends Field {
 
 	/**
 	 * init()
-	 * 
+	 *
 	 * Called when the field is initialized. (back-end, front-end)
-	 * Useful for hooking up to front-end hooks. 
-	 * 
+	 * Useful for hooking up to front-end hooks.
+	 *
 	 */
 	/*
-	function init() {
+	public function init() {
 		parent::init(); // do not delete
 	}
 	*/
