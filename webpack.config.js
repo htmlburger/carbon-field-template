f6eeafdfca3aff9a21208cba74521fe7e537287c
Viktor Vasilev <viktor.2create@gmail.com>
2017-04-13 09:22:46 +0300

    Refactor Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
-/*
-This path assumes that Carbon Fields and the current template
-are both in the /plugins dir. Change it according to your needs
-*/
+
+/**
+ * This path assumes that Carbon Fields and the current template
+ * are both in the `/plugins` directory. Change if according to your needs.
+ */
 const root = path.resolve(__dirname, '../carbon-fields');
 
+if (!fs.existsSync(root)) {
+	console.error('Could not find Carbon Fields folder.');
+	process.exit(1);
+	return;
+}
+
 module.exports = {
-	context: __dirname,
-	entry: './assets/react/bootstrap.js',
+	entry: './assets/js/bootstrap.js',
+
 	output: {
-		path: path.resolve(__dirname, 'assets/js/'),
+		path: path.resolve(__dirname, 'assets/dist'),
 		filename: 'bundle.js'
 	},
 
@@ -18,32 +26,43 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				loader: 'babel-loader?cacheDirectory=true',
-				exclude: /node_modules/
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					cacheDirectory: true
+				}
 			}
 		]
 	},
 
 	resolve: {
 		modules: [
-			path.resolve(root, 'assets/react'),
-			path.resolve(__dirname, 'assets/react'),
+			path.resolve(__dirname, 'assets/js'),
+			path.resolve(root, 'assets/js'),
 			'node_modules'
 		]
 	},
 
-	devtool: 'cheap-module-source-map',
-
 	plugins: [
+		new webpack.ProvidePlugin({
+			'jQuery': 'jquery'
+		}),
+
 		new webpack.DllReferencePlugin({
 			sourceType: 'this',
-			manifest: require(path.resolve(root, 'assets/carbon.vendor.json'))
+			manifest: require(path.resolve(root, 'assets/dist/carbon.vendor.json')),
 		}),
 
 		new webpack.DllReferencePlugin({
 			context: root,
 			sourceType: 'this',
-			manifest: require(path.resolve(root, 'assets/carbon.core.json'))
+			manifest: require(path.resolve(root, 'assets/dist/carbon.core.json'))
 		})
 	],
+
+	externals: {
+		'jquery': 'jQuery'
+	},
+
+	devtool: '#cheap-module-eval-source-map'
 };
