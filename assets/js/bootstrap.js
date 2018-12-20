ca730e78a570c8d209cf832c0a380ffcd75f0eea
ggeorgiev <ggeorgiev@htmlbox.com>
2018-12-20 17:09:55 +0200

    Remove 'assets' folder from the repo

--- assets/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-/**
- * The internal dependencies.
- */
-import { registerFieldType } from 'core/registry/fields';
-import YOURFIELDNAMEField from 'components/field';
-
-registerFieldType( 'yourfieldname', YOURFIELDNAMEField );
